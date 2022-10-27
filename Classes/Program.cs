084ade2b21ca48b7f5d4eca8ca7c45065aa58edc
koikiri <kahvisabatino@gmail.com>
2022-10-27 09:55:52 -1000

    Submit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car aCuteCar = new Car();
+            Car aCuteCar2 = new Car();
+            Car aCuteCar3 = new Car();
+
+            aCuteCar.Name = "Mini";
+            aCuteCar2.Name = "Aceman";
+            aCuteCar3.Name = "2022";
+
+            Console.Write(aCuteCar.Name);
+            Console.WriteLine(aCuteCar2.Name);
+            Console.WriteLine(aCuteCar3.Name);
+
         }
     }
 }
